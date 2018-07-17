9f9a0451cefe17ee9202fc536d453e4f9e12bad5
Rhododendron1210 <lea.louesdon@gmail.com>
2018-07-17 14:12:11 +0200

    Start

---
+++ js/index.js
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+//gloabl
+var max = 100;
+var courbe = document.getElementById("Courbe");
+courbe.setAttribute(
+  "d",
+  "M 1,602 C 114,500,158,500,270,500 S 411,500,541,500,683,500,811,500 S 1080, 602, 1080, 602 H0Z"
+);
+//document.getElementById("body").style["width"] = screen.width;
+//document.getElementById("body").style["height"] = screen.height;
+//slider 1
+var slider1 = document.getElementById("myRange1");
+var output1 = document.getElementById("s1");
+output1.innerHTML = slider1.value;
+var c = 500;
+
+slider1.oninput = function() {
+  var oldValue = Number(output1.innerHTML);
+  if (Number(slider2.value) + Number(this.value) > max) {
+    this.value = oldValue;
+  } else {
+    output1.innerHTML = this.value;
+    calcVal();
+    courbeSvg();
+  }
+};
+//slider 2
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("s2");
+output2.innerHTML = slider2.value;
+slider2.oninput = function() {
+  var oldValue = Number(output2.innerHTML);
+  if (Number(slider1.value) + Number(this.value) > max) {
+    this.value = oldValue;
+  } else {
+    output2.innerHTML = this.value;
+    calcVal();
+    courbeSvg();
+  }
+};
+//slider 3
+var slider3 = document.getElementById("myRange3");
+var output3 = document.getElementById("s3");
+output3.innerHTML = slider3.value;
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+};
+
+function calcVal() {
+  slider3.value = max - (Number(slider1.value) + Number(slider2.value));
+  output3.innerHTML = slider3.value;
+  courbeSvg();
+}
+
+function courbeSvg() {
+  var modif =
+    "M 1,602 C 114, 500, 158, " +
+    (Number(slider1.value) - 100) * -5 +
+    ", 270, " +
+    (Number(slider1.value) - 100) * -5 +
+    " S 411, " +
+    (Number(slider2.value) - 100) * -5 +
+    ", 541, " +
+    (Number(slider2.value) - 100) * -5 +
+    ", 683," +
+    (Number(slider3.value) - 100) * -5 +
+    ", 811, " +
+    (Number(slider3.value) - 100) * -5 +
+    " S 1080, 602, 1080, 602 H0Z";
+  courbe.setAttribute("d", modif);
+}
